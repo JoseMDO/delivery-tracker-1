3c09c720a5aa56761438cb25888daa4bac4182cb
Jose Montes De Oca Morfin <112668683+JoseMDO@users.noreply.github.com>
2024-02-14 16:07:57 +0000

    finished

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,8 +1,14 @@
 class DeliveriesController < ApplicationController
   def index
-    matching_deliveries = Delivery.all
+    matching_waiting_on_deliveries = Delivery.where({ :user_id => current_user.id }).where({ :arrived => false })
 
-    @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+    @list_of_waiting_on_deliveries = matching_waiting_on_deliveries.order({ :created_at => :desc })
+
+    matching_recieved_deliveries = Delivery.where({ :arrived => true }).where({ :user_id => current_user.id })
+
+    @list_of_recieved_deliveries = matching_recieved_deliveries.order({ :created_at => :desc })
+
+
 
     render({ :template => "deliveries/index" })
   end
@@ -19,14 +25,15 @@ class DeliveriesController < ApplicationController
 
   def create
     the_delivery = Delivery.new
-    the_delivery.user_id = params.fetch("query_user_id")
+    the_delivery.user_id = current_user.id
     the_delivery.description = params.fetch("query_description")
     the_delivery.details = params.fetch("query_details")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
+    the_delivery.arrived = false
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Delivery created successfully." })
+      redirect_to("/deliveries", { :notice => "Added to list" })
     else
       redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
@@ -36,16 +43,17 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.details = params.fetch("query_details")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
+    #the_delivery.user_id = params.fetch("query_user_id")
+    #the_delivery.description = params.fetch("query_description")
+    #the_delivery.details = params.fetch("query_details")
+    #the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
+    the_delivery.arrived = params.fetch("arrived")
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries/#{the_delivery.id}", { :notice => "Delivery updated successfully."} )
+      redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
     else
-      redirect_to("/deliveries/#{the_delivery.id}", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
